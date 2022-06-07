0fa834420de3031dabfac718b0c4a8b86737ec43
VladymyrAlekseevich <SavinVolodya001@gmail.com>
2022-06-07 22:29:06 +0300

    feat: add some basic standart middleware

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const app = express();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const PORT = process.env.PORT || 80;
 
-const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
 const start = async () => {
     app.listen(PORT, () => console.log(`Server started. Listening to port ${PORT}.`));
 }
