8418fc8c8e408137542bb22d83bc49c60803d440
VladymyrAlekseevich <SavinVolodya001@gmail.com>
2022-06-09 22:14:28 +0300

    feat: define endpoints

---
+++ server/Router/index.js
@@ -0,0 +1,13 @@
+const Router = require('express').Router;
+
+const router = new Router(); 
+
+router.post('/registration', );
+router.post('/login', );
+router.post('/logout', );
+router.get('/activate/:link', );
+router.get('/refresh', );
+router.get('/user-data', );
+
+module.exports = router;
+
--- server/index.js
+++ server/index.js
@@ -4,11 +4,14 @@ const app = express();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
+const router = require('./Router/index');
+
 const PORT = process.env.PORT || 80;
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use('/api', router);
 
 const start = async () => {
     app.listen(PORT, () => console.log(`Server started. Listening to port ${PORT}.`));
