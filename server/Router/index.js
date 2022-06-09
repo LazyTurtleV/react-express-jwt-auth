a291c1a3b27fddc24729c5e8a0fc19d2efcc8f08
VladymyrAlekseevich <SavinVolodya001@gmail.com>
2022-06-09 22:18:32 +0300

    feat: initial UserController structure

--- server/Router/index.js
+++ server/Router/index.js
@@ -1,13 +1,14 @@
 const Router = require('express').Router;
 
 const router = new Router(); 
+const userController = require('../Controllers/UserController');
 
-router.post('/registration', );
-router.post('/login', );
-router.post('/logout', );
-router.get('/activate/:link', );
-router.get('/refresh', );
-router.get('/user-data', );
+router.post('/registration', userController.register );
+router.post('/login', userController.login);
+router.post('/logout', userController.logout);
+router.get('/activate/:link', userController.activate);
+router.get('/refresh', userController.refresh);
+router.get('/user-data', userController.getUserData);
 
 module.exports = router;
 
