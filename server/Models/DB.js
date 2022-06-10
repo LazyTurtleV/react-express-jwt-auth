3a8caf0deae7285878f6737b87ea5f31f656a741
VladymyrAlekseevich <SavinVolodya001@gmail.com>
2022-06-10 22:31:50 +0300

    feat: add methods to model

--- server/Models/DB.js
+++ server/Models/DB.js
@@ -15,6 +15,29 @@ class DBModel {
         }
     }
 
+    constructor(){
+        this.init();
+    }
+
+    //determines if the email  exists on DB
+    async validateEmail(email) {
+        if (!this.connection) await this.init();
+
+        const query = `call spiValidateEmail(?)`;
+        const [ r, f ] = await this.connection.query(query, [email]);
+
+        //some weird response structure determines this magic code
+        const rowName = f[0][0].name;
+        return !!r[0][0][rowName];
+    }
+
+    async createUser(email, password) {
+        if (!this.connection) await this.init();
+
+        const query = 'call spiCreateUser(?, ?)';
+        await this.connection.query(query, [email, password]);
+    }
+
     async closeConnection () {
         await this.connection.end();
         this.connection = undefined;
