495f105bddfd7b494cfb3a1b7842fa34788788c5
VladymyrAlekseevich <SavinVolodya001@gmail.com>
2022-07-06 22:27:33 +0300

    feat: add registration logic

--- server/Services/TockenService.js
+++ server/Services/TockenService.js
@@ -1,5 +1,23 @@
+const jwt = require('jsonwebtoken');
+const DB = require('../Models/DB');
 class TockenService {
+    generateToken(payload) {
+        const accessToken = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '15m' });
+        const refreshToken = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '30d' });
 
+        return {
+            accessToken,
+            refreshToken
+        }
+    }
+
+    async updateRefreshToken(userId, refreshToken) {
+        try {
+            await DB.updateUserRefreshToken(userId, refreshToken);
+        } catch (e) {
+            console.log('SOME ERR OCCURED WHILE UPDATING TOKEN', e);
+        }
+    }
 }
 
 module.exports = new TockenService();
--- server/Services/UserService.js
+++ server/Services/UserService.js
@@ -1,5 +1,33 @@
-class UserService {
+const bcrypt = require('bcrypt');
+const uuid = require('uuid'); 
 
+const dbModel = require('../Models/DB');
+const mailService = require('./MailService');
+const tokenService = require('./TockenService');
+class UserService {
+    async register(email, password) {
+        const absent = await dbModel.validateEmail(email);
+        if (!absent) {
+            throw new Error('The email already exists');
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 3);
+        const activationLink = uuid.v4();
+        const userId = await dbModel.createUser(email, hashedPassword, activationLink);
+        console.log('user id', userId);
+        
+        await mailService.sendActivationMail(email, activationLink);
+        const tokens = tokenService.generateToken({ email })
+        
+        tokenService.updateRefreshToken(userId, tokens.refreshToken);
+
+        return {
+            ...tokens,
+            user: {
+                userId
+            }
+        }
+    }
 }
 
 module.exports = new UserService();
