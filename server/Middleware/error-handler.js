11946d5baefd7ca0418b4dcb245ecaefc40ba7da
VladymyrAlekseevich <SavinVolodya001@gmail.com>
2022-07-07 21:50:27 +0300

    fix: header set after response send

--- server/Middleware/error-handler.js
+++ server/Middleware/error-handler.js
@@ -3,7 +3,7 @@ const HttpError = require('../Exceptions/http-error');
 module.exports = function (err, req, res, next) {
     console.log(err);
     if (err instanceof HttpError) {
-        res.status(err.status).json({ message: err.message, errors: err.errors });
+        return res.status(err.status).json({ message: err.message, errors: err.errors });
     }
 
     return res.status(500).json({ message: 'Internal server error' });
