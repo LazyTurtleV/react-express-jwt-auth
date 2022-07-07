2e520442c77913475183cddd57a2620c9fb460f7
VladymyrAlekseevich <SavinVolodya001@gmail.com>
2022-07-07 20:32:16 +0300

    feat: implement MailService

--- server/Services/MailService.js
+++ server/Services/MailService.js
@@ -1,5 +1,34 @@
+const mailer = require('nodemailer');
 class MailService {
-    async sendActivationMail(to, link) {}
+    constructor() {
+        this.transporter = mailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secureConnection: false,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASSWORD,
+            },
+            tls: {
+                ciphers:'SSLv3'
+            }
+        })
+    }
+
+    async sendActivationMail(to, link) {
+        await this.transporter.sendMail({
+            from: process.env.SMTP_USER,
+            to: to,
+            subject: 'Account activation',
+            text: '',
+            html: `
+                <div>
+                    <h1>For the activation visit the following link</h1>
+                    <a href="${link}">${link}<a/>    
+                </div>
+            `
+        })
+    }
 }
 
 module.exports = new MailService();
