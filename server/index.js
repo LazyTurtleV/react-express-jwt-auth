399a77b76c14abd8ce6f1be959db3f21d2ffb238
VladymyrAlekseevich <SavinVolodya001@gmail.com>
2022-07-12 21:36:41 +0300

    fix: add cors config for credentials include mode

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 80;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use('/api', router);
 app.use(errorMiddleware);
 
