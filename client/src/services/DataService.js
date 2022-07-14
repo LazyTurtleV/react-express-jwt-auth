abbdb70a6ed428d52c12eefa04a43fcb00de5a7d
VladymyrAlekseevich <SavinVolodya001@gmail.com>
2022-07-14 20:50:20 +0300

    refactor: take config methods out to separate class

--- client/src/services/DataService.js
+++ client/src/services/DataService.js
@@ -1,34 +1,6 @@
 import { getDefaultFetchConfig } from '../utils/http';
-class DataService {
-    constructor() {
-        //otherwise this is undefined inside the function
-        this.handleRequest = this.handleRequest.bind(this);
-    }
-
-    init({ apiBase }) {
-        this.apiBase = apiBase;
-    }
-
-    errorHandlers = [];
-
-    async handleRequest(r) {
-        if (r.status) {
-            console.log('ERR CAUGHT', r.status, r.statusText);
-
-            for (const handler of this.errorHandlers) {
-                await handler(r);
-            }
-
-            return;
-        }
-
-        return r.json();
-    }
-
-    subscribeOnError(callback) {
-        this.errorHandlers.push(callback);
-    }
-
+import HttpService from './HttpService';
+class DataService extends HttpService {
     async getData(accessToken) {
         return fetch(`${this.apiBase}/user-data`, {
             method: 'GET',
---
+++ client/src/services/HttpService.js
@@ -0,0 +1,32 @@
+class HttpService {
+    constructor() {
+        //otherwise this is undefined inside the function
+        this.handleRequest = this.handleRequest.bind(this);
+    }
+
+    init({ apiBase }) {
+        this.apiBase = apiBase;
+    }
+
+    errorHandlers = [];
+
+    async handleRequest(r) {
+        if (r.status) {
+            console.log('ERR CAUGHT', r.status, r.statusText);
+
+            for (const handler of this.errorHandlers) {
+                await handler(r);
+            }
+
+            return;
+        }
+
+        return r.json();
+    }
+
+    subscribeOnError(callback) {
+        this.errorHandlers.push(callback);
+    }
+}
+
+export default HttpService;
