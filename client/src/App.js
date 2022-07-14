51be2aea601e1d463d667880c0bc5687fe23029f
VladymyrAlekseevich <SavinVolodya001@gmail.com>
2022-07-14 21:23:46 +0300

    feat: add auth service

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from "react";
 
 import config from './config/config.json';
+import AuthService from "./services/AuthService";
 import DataService from "./services/DataService";
 
 
 function App() {
   useEffect(() => {
     DataService.init(config);
+    AuthService.init(config);
   },[]);
 
   return (
---
+++ client/src/services/AuthService.js
@@ -0,0 +1,62 @@
+import Cookie from 'js-cookie';
+
+import { getDefaultFetchConfig } from '../utils/http';
+import HttpService from './HttpService';
+
+class AuthService extends HttpService {
+    constructor(){
+        super();
+        this.token = Cookie.get('token');
+    }
+
+    saveAccessToken(token) {
+        Cookie.set('token', token);
+        this.token = token;
+    }
+
+    getToken() {
+        return this.token;
+    }
+
+    async registration(email, password) {
+        const { accessToken } = await fetch(`${this.apiBase}/registration`, {
+            method: 'POST',
+            ...getDefaultFetchConfig(''),
+            body: JSON.stringify({
+                email,
+                password,
+            }),
+        }).then(this.handleRequest)
+        this.saveAccessToken(accessToken);
+    }
+
+    async login(email, password) {
+        const { accessToken } = await fetch(`${this.apiBase}/login`, {
+            method: 'POST',
+            ...getDefaultFetchConfig(''),
+            body: JSON.stringify({
+                email,
+                password,
+            }),
+        }).then(this.handleRequest);
+        this.saveAccessToken(accessToken);
+    }
+
+    async logout() {
+        await fetch(`${this.apiBase}/logout`, {
+            method: 'POST',
+            ...getDefaultFetchConfig(''),
+            body: JSON.stringify({}),
+        }).then(this.handleRequest);
+    }
+
+    async refresh() {
+        const { accessToken } = await fetch(`${this.apiBase}/refresh`, {
+            method: 'GET',
+            ...getDefaultFetchConfig(this.token),
+        }).then(this.handleRequest);
+        this.saveAccessToken(accessToken);
+    }
+};
+
+export default new AuthService();
