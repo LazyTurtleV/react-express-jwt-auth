e18f337680ceae5b1d9bf4d240418273da365c90
VladymyrAlekseevich <SavinVolodya001@gmail.com>
2022-07-15 19:58:44 +0300

    feat: define user component

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,2 +1,12 @@
+import { useEffect, useState } from "react";
+import DataService from "../services/DataService";
+
 export default function UserData() {
+    const [data, setData] = useState();
+    
+    useEffect(() => {
+        DataService.getData().then((d) => setData(d));
+    })
+
+    return <div>{data}</div>
 }
